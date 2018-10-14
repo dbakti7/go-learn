919fd3f3ff50957eabefb5378840f30771f6a84a
dbakti7 <dbakti1605@gmail.com>
2018-10-14 10:40:45 +0800

    Added go dep into travis.yml

--- checker/checker.go
+++ checker/checker.go
@@ -1,1 +1,9 @@
 package checker
+
+import (
+	"github.com/satori/go.uuid"
+)
+
+func check() {
+	uuid.NewV4()
+}
---
+++ checker/checker_test.go
@@ -0,0 +1,11 @@
+package checker
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPackageImport(t *testing.T) {
+	assert.Equal(t, 1, 1, "Numbers should be equal")
+}
