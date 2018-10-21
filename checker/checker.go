ab088d56354745b8cdf8aa4633442f4b372870dc
dbakti7 <dbakti1605@gmail.com>
2018-10-22 01:01:11 +0800

    Added common package; Added basic checker implementation.

--- checker/checker.go
+++ checker/checker.go
@@ -1,9 +1,76 @@
 package checker
 
 import (
-	"github.com/satori/go.uuid"
+	"strings"
+
+	"github.com/dbakti7/go-learn/common"
 )
 
-func check() {
-	uuid.NewV4()
+func StrictCheck(expectedPath string, actualPath string) (accepted bool, err error) {
+	expected, err := common.ReadLines(expectedPath)
+	if err != nil {
+		return
+	}
+
+	actual, err := common.ReadLines(actualPath)
+	if err != nil {
+		return
+	}
+
+	if len(expected) != len(actual) {
+		return
+	}
+
+	for i := range expected {
+		// strings retrieved from Read function might contain newline character
+		if expected[i] != actual[i] {
+			return
+		}
+	}
+	accepted = true
+	return
+}
+
+func LenientCheck(expectedPath string, actualPath string) (accepted bool, err error) {
+	expected, err := common.ReadLines(expectedPath)
+	if err != nil {
+		return
+	}
+
+	actual, err := common.ReadLines(actualPath)
+	if err != nil {
+		return
+	}
+
+	preprocess := func(lines []string) []string {
+		// remove leading and trailing white spaces
+		for i := range lines {
+			lines[i] = strings.TrimSpace(lines[i])
+		}
+
+		// remove trailing empty lines
+		for i := len(lines) - 1; i >= 0; i-- {
+			if lines[i] != "" {
+				lines = lines[:i+1]
+				break
+			}
+		}
+		return lines
+	}
+
+	expected = preprocess(expected)
+	actual = preprocess(actual)
+
+	if len(expected) != len(actual) {
+		return
+	}
+
+	for i := range expected {
+		// strings retrieved from Read function might contain newline character
+		if expected[i] != actual[i] {
+			return
+		}
+	}
+	accepted = true
+	return
 }
--- checker/checker_test.go
+++ checker/checker_test.go
@@ -1,11 +1,83 @@
 package checker
 
 import (
+	"os"
 	"testing"
+
+	"github.com/dbakti7/go-learn/common"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPackageImport(t *testing.T) {
-	assert.Equal(t, 1, 1, "Numbers should be equal")
+func TestStrictCheck(t *testing.T) {
+	expected := []string{"aa", "bb", "", "123"}
+	common.WriteLines(expected, "expected.txt")
+	common.WriteLines(expected, "actualAccepted.txt")
+	defer os.Remove("expected.txt")
+	defer os.Remove("actualAccepted.txt")
+
+	accepted, err := StrictCheck("expected.txt", "actualAccepted.txt")
+	assert.Equal(t, nil, err, "There should not be error.")
+	assert.Equal(t, true, accepted, "Should be accepted.")
+
+	common.WriteLines(append(expected, ""), "actualWrongAnswer.txt")
+	defer os.Remove("actualWrongAnswer.txt")
+
+	accepted, err = StrictCheck("expected.txt", "actualWrongAnswer.txt")
+	assert.Equal(t, nil, err, "There should not be error.")
+	assert.Equal(t, false, accepted, "Extra line should not be accepted.")
+
+	expected[0] = "ab"
+	common.WriteLines(expected, "actualWrongAnswer.txt")
+	accepted, err = StrictCheck("expected.txt", "actualWrongAnswer.txt")
+	assert.Equal(t, nil, err, "There should not be error.")
+	assert.Equal(t, false, accepted, "Wrong output should not be accepted")
+
+	expected[0] = "aa "
+	common.WriteLines(expected, "actualWrongAnswer.txt")
+	accepted, err = StrictCheck("expected.txt", "actualWrongAnswer.txt")
+	assert.Equal(t, nil, err, "There should not be error.")
+	assert.Equal(t, false, accepted, "Trailing spaces should not be accepted")
+
+	expected[0] = " aa"
+	common.WriteLines(expected, "actualWrongAnswer.txt")
+	accepted, err = StrictCheck("expected.txt", "actualWrongAnswer.txt")
+	assert.Equal(t, nil, err, "There should not be error.")
+	assert.Equal(t, false, accepted, "Leading spaces should not be accepted")
+}
+
+func TestLenientCheck(t *testing.T) {
+	expected := []string{"aa", "bb", "", "123"}
+	common.WriteLines(expected, "expected.txt")
+	common.WriteLines(expected, "actualAccepted.txt")
+	defer os.Remove("expected.txt")
+	defer os.Remove("actualAccepted.txt")
+
+	accepted, err := LenientCheck("expected.txt", "actualAccepted.txt")
+	assert.Equal(t, nil, err, "There should not be error.")
+	assert.Equal(t, true, accepted, "Should be accepted.")
+
+	common.WriteLines(append(expected, "  ", "\n"), "actualAccepted.txt")
+	accepted, err = LenientCheck("expected.txt", "actualAccepted.txt")
+	assert.Equal(t, nil, err, "There should not be error.")
+	assert.Equal(t, true, accepted, "Extra empty lines should be accepted.")
+
+	expected[0] = "ab"
+	common.WriteLines(expected, "actualWrongAnswer.txt")
+	defer os.Remove("actualWrongAnswer.txt")
+	accepted, err = LenientCheck("expected.txt", "actualWrongAnswer.txt")
+	assert.Equal(t, nil, err, "There should not be error.")
+	assert.Equal(t, false, accepted, "Wrong output should not be accepted")
+
+	expected[0] = "aa "
+	common.WriteLines(expected, "actualAccepted.txt")
+	accepted, err = LenientCheck("expected.txt", "actualAccepted.txt")
+	assert.Equal(t, nil, err, "There should not be error.")
+	assert.Equal(t, true, accepted, "Trailing spaces should be accepted")
+
+	expected[0] = " aa"
+	common.WriteLines(expected, "actualAccepted.txt")
+	accepted, err = LenientCheck("expected.txt", "actualAccepted.txt")
+	assert.Equal(t, nil, err, "There should not be error.")
+	assert.Equal(t, true, accepted, "Leading spaces should be accepted")
 }
